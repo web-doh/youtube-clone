a77cdc32f8746ecd732c441e25265b1fb4b941db
web-doh <66818228+web-doh@users.noreply.github.com>
2020-12-24 23:05:57 +0900

    setting project

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import "./app.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
+
+function App() {}
+
+export default App;
