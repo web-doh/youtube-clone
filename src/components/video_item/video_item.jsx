d75e51cdabc3cc672271ac268861f6f785fac21b
web-doh <66818228+web-doh@users.noreply.github.com>
2020-12-25 01:25:16 +0900

    2. make video item summary

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = ({ video: { snippet } }) => {
+const VideoItem = ({
+  video: { snippet },
+  video: {
+    snippet: { thumbnails },
+  },
+}) => {
+  const regex = /-/g;
+  const date = snippet.publishedAt.split("T")[0].replace(regex, ". ");
+
   return (
     <li className={styles.item}>
-      <h1>{snippet.title}</h1>
+      <img
+        className={styles.thumbnail}
+        src={thumbnails.medium.url}
+        alt="video thumbnail"
+        title={snippet.title}
+      />
+      <div className={styles.body}>
+        <h2 className={styles.title}>{snippet.title}</h2>
+        <p className={styles.channelTitle}>{snippet.channelTitle}</p>
+        <p className={styles.publishedAt}>{date}</p>
+      </div>
     </li>
   );
 };
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
+import styles from "./video_list.module.css";
 
 const VideoList = ({ videos }) => {
   return (
-    <ul>
+    <ul className={styles.list}>
       {videos.map((video) => (
         <VideoItem video={video} key={video.id} />
       ))}
